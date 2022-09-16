c8c3f30d36e3e5d4da2fcd94b862e06e6ebe6274
pan-alex <alexanderypan@gmail.com>
2022-09-15 22:46:23 -0400

    docs: update function docstrings

--- public/js/main.js
+++ public/js/main.js
@@ -26,21 +26,13 @@ function calculateShare(group) {
 }
 
 
-// Determine the best way to allocate funds (minimum number of transactions)
-
-// Likely a greedy approach is easiest:
-    // Person with least credit pays out to the person with most credit.
-        // This person continues paying out until all others are at 0.
-    // Person with the next least credit pays out the person with the most credit remaining
-    // Repeat until all credits are 0.
-// Return an array with person payer, person receiver to, amount.
-// [
-//   [A, B, 15] -> A pays B 15.
-//   [C, D, 5] -> C pays D 5.
-// ]
-
 function calculateRepayments(group) {
-    // People, sorted by most payer to least payer.
+    // Parameters:
+        // group: Object where each property is an object representing a person.
+    // Returns:
+        // repayments: Array representing the repayments each person should make so that no one owes anyone money.
+            // Each element is a subarray representing one payment. It has the format [P, R, Amt] where P = payer, R = receiver, Amt = amount to pay.
+            // A greedy approach is utilized where the person with the least credit pays the person with the most credit until all debts are paid.
     let people = []
     for (person in group) {
         people.push([person, group[person].credit]) // format: [name, credit]
@@ -48,7 +40,6 @@ function calculateRepayments(group) {
     payers = people.filter( a => a[1] < 0).sort( (a,b) => a[1] - b[1]); // Sort lowest to highest credit
     receivers  = people.filter( a => a[1] > 0).sort( (a,b) => b[1] - a[1]); // Sort highest to lowest credit
 
-    // Calculate repayments
     let repayments = []
     do {
         // Pay off the person with the most credit
@@ -56,24 +47,14 @@ function calculateRepayments(group) {
         let receiver = receivers[0];
         let diff = +receiver[1] + +payer[1]
         if (diff < 0) {
-            // If payer owes more than receiver is owed:
-                // receiver is paid all credit
-                // receiver credit is added to payer credit.
-                // receiver is removed as credit is 0.
             payer[1] =  m(payer[1] + receiver[1]);
             repayments.push( [payer[0], receiver[0], m(receiver[1])] )
             receivers.shift()
         } else if (diff > 0) {
-            // If payer owes less than receiver is owed:
-                // payer pays off all debt
-                // payer payment is subtracted from receiver credit
-                // payer is removed as credit is 0.
             receiver[1] = m(receiver[1] + payer[1]);
             repayments.push( [payer[0], receiver[0], m(-payer[1])] )
             payers.shift()
         } else {
-            // If amount is equal
-                // No math needed; remove both payer and receiver as their credit is 0.
             repayments.push( [payer[0], receiver[0], m(receiver[1])] )
             receivers.shift()
             payers.shift()
