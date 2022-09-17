d288d0423f55bc3f7b299376b80007eaa67d0725
pan-alex <alexanderypan@gmail.com>
2022-09-16 22:24:52 -0400

    change: to OOP-based tests

--- public/js/tests.js
+++ public/js/tests.js
@@ -1,23 +1,25 @@
-group1 = {
-    'A': {'contribution': 20, 'split': 0.5},
-    'B': {'contribution': 10, 'split': 0.5}
-}
-group1 = calculateShare(group1)
-payments1 = calculateRepayments(group1)
+group1 = new Group()
+group1.add('A', 0.5)
+group1.add('B', 0.5)
+group1['A'].addTransaction(20)
+group1['B'].addTransaction(10)
+group1.calculateRepayments()
 
-group2 = {
-    'A': {'contribution': 20, 'split': 20},
-    'B': {'contribution': 5, 'split': 20},
-    'C': {'contribution': 10, 'split': 20},
-}
-group2 = calculateShare(group2)
-payments2 = calculateRepayments(group2)
+group2 = new Group()
+group2.add('A', 20)
+group2.add('B', 20)
+group2.add('C', 20)
+group2['A'].addTransaction(20)
+group2['B'].addTransaction(5)
+group2['C'].addTransaction(10)
+group2.calculateRepayments()
 
-group3 = {
-    'A': {'contribution': 20, 'split': 20},
-    'B': {'contribution': 10, 'split': 20},
-    'C': {'contribution': 0, 'split': 40},
-    'D': {'contribution': 40, 'split': 15}
-}
-group3 = calculateShare(group3)
-payments3 = calculateRepayments(group3)
+group3 = new Group()
+group3.add('A', 20)
+group3.add('B', 20)
+group3.add('C', 40)
+group3.add('D', 15)
+group3['A'].addTransaction(20)
+group3['B'].addTransaction(10)
+group3['D'].addTransaction(40)
+group3.calculateRepayments()
