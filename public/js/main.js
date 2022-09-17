1d02ef9a4bce43d7716c422869af348052fa5088
pan-alex <alexanderypan@gmail.com>
2022-09-16 23:06:58 -0400

    feat: Add method to cast objects to Group/Person class
    - Add method to cast objects to Group/Person class
    - Pull calculateShare and calculateRepayments out of the Group class into stand alone functions (for now)
    - Add test coverage

--- public/js/main.js
+++ public/js/main.js
@@ -16,58 +16,17 @@ class Group {
             alert('That name is already being used.')
         }
     }
+
     delete(name) {
         delete this[name]
     }
 
-    calculateShare() {
-        // Returns:
-        // this: Modifies the input object to add the following property:
-            // credit: Number. The amount receiver to the this, based on their contribution minus what they are responsible for (sum * split).
-                // The sum of credit for all perople is 0.
-            // split: Modifies this number to reflect a percentage of the total group.
-        let sum = Object.values(this).reduce( (sum, person) => sum + person.contribution, 0);
-        let splitSum = Object.values(this).reduce( (splitSum, person) => splitSum + person.split, 0);
-        for (let person in this) {
-            this[person].split = this[person].split / splitSum
-            this[person].credit = m(this[person].contribution - sum * this[person].split)
+    objToGroup(obj) {
+        obj = Object.assign(new Group(), obj)
+        for (let person in obj) {
+            obj[person] = Object.assign(new Person(), obj[person])
         }
-        return this
-     }
-    calculateRepayments() {
-        // Returns:
-            // repayments: Array representing the repayments each person should make so that no one owes anyone money.
-                // Each element is a subarray representing one payment. It has the format [P, R, Amt] where P = payer, R = receiver, Amt = amount to pay.
-                // A greedy approach is utilized where the person with the least credit pays the person with the most credit until all debts are paid.
-        this.calculateShare()
-        let people = []
-        for (let person in this) {
-            people.push([person, this[person].credit]) // format: [name, credit]
-        }
-        let payers = people.filter( a => a[1] < 0).sort( (a,b) => a[1] - b[1]);
-        let receivers  = people.filter( a => a[1] > 0).sort( (a,b) => b[1] - a[1]);
-
-        let repayments = []
-        do {
-            // Pay off the person with the most credit
-            let payer = payers[0];
-            let receiver = receivers[0];
-            let diff = +receiver[1] + +payer[1]
-            if (diff < 0) {
-                payer[1] =  m(payer[1] + receiver[1]);
-                repayments.push( [payer[0], receiver[0], m(receiver[1])] )
-                receivers.shift()
-            } else if (diff > 0) {
-                receiver[1] = m(receiver[1] + payer[1]);
-                repayments.push( [payer[0], receiver[0], m(-payer[1])] )
-                payers.shift()
-            } else {
-                repayments.push( [payer[0], receiver[0], m(receiver[1])] )
-                receivers.shift()
-                payers.shift()
-            }
-        } while (payers.length > 0 && receivers.length > 0)
-        return repayments
+        return obj
     }
 }
 
@@ -92,3 +51,54 @@ class Person {
     }
 }
 
+
+function calculateShare(group) {
+    // Returns:
+    // this: Modifies the input object to add the following property:
+        // credit: Number. The amount receiver to the this, based on their contribution minus what they are responsible for (sum * split).
+            // The sum of credit for all perople is 0.
+        // split: Modifies this number to reflect a percentage of the total group.
+    let sum = Object.values(group).reduce( (sum, person) => sum + person.contribution, 0);
+    let splitSum = Object.values(group).reduce( (splitSum, person) => splitSum + person.split, 0);
+    for (let person in group) {
+        group[person].split = group[person].split / splitSum
+        group[person].credit = m(group[person].contribution - sum * group[person].split)
+    }
+    return group
+ }
+
+function calculateRepayments(group) {
+    // Returns:
+        // repayments: Array representing the repayments each person should make so that no one owes anyone money.
+            // Each element is a subarray representing one payment. It has the format [P, R, Amt] where P = payer, R = receiver, Amt = amount to pay.
+            // A greedy approach is utilized where the person with the least credit pays the person with the most credit until all debts are paid.
+    calculateShare(group)
+    let people = []
+    for (let person in group) {
+        people.push([person, group[person].credit]) // format: [name, credit]
+    }
+    let payers = people.filter( a => a[1] < 0).sort( (a,b) => a[1] - b[1]);
+    let receivers  = people.filter( a => a[1] > 0).sort( (a,b) => b[1] - a[1]);
+
+    let repayments = []
+    do {
+        // Pay off the person with the most credit
+        let payer = payers[0];
+        let receiver = receivers[0];
+        let diff = +receiver[1] + +payer[1]
+        if (diff < 0) {
+            payer[1] =  m(payer[1] + receiver[1]);
+            repayments.push( [payer[0], receiver[0], m(receiver[1])] )
+            receivers.shift()
+        } else if (diff > 0) {
+            receiver[1] = m(receiver[1] + payer[1]);
+            repayments.push( [payer[0], receiver[0], m(-payer[1])] )
+            payers.shift()
+        } else {
+            repayments.push( [payer[0], receiver[0], m(receiver[1])] )
+            receivers.shift()
+            payers.shift()
+        }
+    } while (payers.length > 0 && receivers.length > 0)
+    return repayments
+}
--- public/js/tests.js
+++ public/js/tests.js
@@ -1,10 +1,19 @@
+// Simple group
 group1 = new Group()
 group1.add('A', 0.5)
 group1.add('B', 0.5)
 group1['A'].addTransaction(20)
 group1['B'].addTransaction(10)
-group1.calculateRepayments()
+calculateRepayments(group1) // ['B', 'A', 5]
+// Adding transactions
+group1['B'].addTransaction(30) // ['A', 'B', 10]
+calculateRepayments(group1)
+// Updating splits
+group1['B'].changeSplit(1.5)
+calculateRepayments(group1)  // ['B', 'A', 5]
 
+
+// Decimal math with irrational numbers
 group2 = new Group()
 group2.add('A', 20)
 group2.add('B', 20)
@@ -12,8 +21,9 @@ group2.add('C', 20)
 group2['A'].addTransaction(20)
 group2['B'].addTransaction(5)
 group2['C'].addTransaction(10)
-group2.calculateRepayments()
+calculateRepayments(group2)
 
+// Larger group
 group3 = new Group()
 group3.add('A', 20)
 group3.add('B', 20)
@@ -22,4 +32,23 @@ group3.add('D', 15)
 group3['A'].addTransaction(20)
 group3['B'].addTransaction(10)
 group3['D'].addTransaction(40)
-group3.calculateRepayments()
+calculateRepayments(group3)
+
+// Casting an object to group class
+group4 = {
+    'A': {'name': 'A', 'contribution': 20, 'split': 1},
+    'B': {'name': 'B', 'contribution': 10, 'split': 1},
+    'C': {'name': 'C', 'contribution': 100, 'split': 1},
+    'D': {'name': 'D', 'contribution': 40, 'split': 1}
+}
+group4 = Group.prototype.objToGroup(group4)
+// Group class methods work
+group4.add('E', 1)
+group4['E'].addTransaction(20)
+calculateRepayments(group4)
+
+group4.delete('E')
+calculateRepayments(group4)
+// Person class methods work
+group4['A'].addTransaction(10)
+calculateShare(group4) // A contribution: 30
