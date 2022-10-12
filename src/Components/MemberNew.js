7b0eb70138725197ae9ac2f6aebc122ec78b1543
pan-alex <alexanderypan@gmail.com>
2022-10-11 20:51:03 -0400

    refactor: Change from querySelector to useRef()

--- src/Components/MemberNew.js
+++ src/Components/MemberNew.js
@@ -1,11 +1,14 @@
 // Menu with fields to create a new member
 import { group } from './Classes'
+import { useRef } from 'react'
 
 export default function MemberNew( {setMembersState} ) {
+
+    const memberName = useRef();
+    const memberSplit = useRef();
+
     function handleNewMember() {
-        let name = document.querySelector('#memberName').value
-        let split = document.querySelector('#memberSplit').value
-        group.addMember(name, split)
+        group.addMember(memberName.current.value, memberSplit.current.value);
         setMembersState()
     }
 
@@ -14,12 +17,12 @@ export default function MemberNew( {setMembersState} ) {
             <h2>Create a new Person</h2>
             <div className="container-lg flex-form">
                 <div  className='form-floating'>
-                    <input type="text" id="memberName" className="form-control" name="memberName" placeholder="name" />
-                    <label htmlFor="memberName">Name</label>
+                    <input type="text" ref={memberName} id="newMemberName" className="form-control" name="memberName" placeholder="name" />
+                    <label htmlFor="newMemberName">Name</label>
                 </div>
                 <div className='form-floating'>
-                <input type="text" id="memberSplit" name="personSplit" placeholder="percent" className="form-control" />
-                    <label htmlFor="memberSplit">Percent</label>
+                <input type="text" ref={memberSplit} id="newMemberSplit" name="personSplit" placeholder="percent" className="form-control" />
+                    <label htmlFor="newMemberSplit">Percent</label>
                 </div>
                 <button id="btnMemberNew" className="btn btn-primary" onClick={handleNewMember}>
                     Add Member
