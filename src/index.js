1bf6faec333edafaa8cbf569ab9622cf19bd2567
pan-alex <alexanderypan@gmail.com>
2022-10-15 16:02:59 -0400

    bugfix: Remove Chakra css reset

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider resetCSS={false}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
