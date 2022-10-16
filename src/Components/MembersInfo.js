aec9f59288eda456d29cafd3f418bd030b901201
pan-alex <alexanderypan@gmail.com>
2022-10-16 11:20:51 -0400

    feat: Add create new member button

--- src/Components/MembersInfo.js
+++ src/Components/MembersInfo.js
@@ -5,23 +5,34 @@ import MemberInfo from './MemberInfo'
 import MemberNew from './MemberNew'
 // import { useState } from 'react'
 
+
 export default function MembersInfo( {setMembersState} ) {
     const memberInfo = group.members.map(member => {
         return (
             <MemberInfo
                 member={member}
                 setMembersState={setMembersState}
-                key={member.name}
+                key={member.id}
             />
         )
     })
+
+    function handleNewMember() {
+        group.addMember('AAA')
+        setMembersState()
+    }
 
     return (
         <>
             <div className="accordion">
                 { memberInfo }
             </div>
-            <MemberNew setMembersState={setMembersState} />
+            <section className="newMember">
+                {/* <IconButton icon='+'/> */}
+                <button arial-label='Add new member' onClick={handleNewMember}>+</button>
+            </section>
+
+            {/* <MemberNew setMembersState={setMembersState} /> */}
         </>
     )
 }
